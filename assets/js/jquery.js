1e56b3b06b0b92f5dce35de75a328f70f8883fb7
Andrés Gallardo <andresgallardo95@gmail.com>
2024-07-16 13:32:05 -0400

    mejora de comentarios en js

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,4 +1,6 @@
-// Inicialización de tooltips de Bootstrap
+// Inicialización de tooltips de Bootstrap cuando el documento está listo
 $(function () {
+    // Selecciona todos los elementos que tienen el atributo 'data-toggle="tooltip"'
+    // y les aplica el método tooltip() para habilitar los tooltips
     $('[data-toggle="tooltip"]').tooltip();
 });
